5025df3d0b9634d94c80c6165889f0e37c44cb6e
Abdilahi khaliif Hassan <daarood11@hotmail.com>
2021-06-01 08:33:28 +0200

    ReactMath

---
+++ src/Component/StaticCalc.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class StaticCalc extends Component {
+
+
+    state ={
+        NumA:3,
+        NumB:6,
+    
+    }
+    render() {
+        return (
+            <div>
+                 <h1>From static File</h1>
+            <p>NumA:{this.state.NumA}</p>
+            <p>NunB:{this.state.NumB}</p>
+            <p>Result: {this.state.NumB + this.state.NumA}</p>
+        
+            </div>
+        );
+    }
+}
+
+export default StaticCalc;
+
+
+
